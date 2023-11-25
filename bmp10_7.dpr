2cc1b8138937247a9d7d537c0b07560c49f0a39a
vip690 <vip.orlova69@mail.ru>
2023-11-25 16:18:38 +0400

    вроде исправил маску

---
+++ bmp10_7.dpr
@@ -0,0 +1,13 @@
+program bmp10_7;
+
+uses
+  Forms,
+  Unit10_7bmp in 'Unit10_7bmp.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
